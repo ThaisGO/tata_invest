99d0c53612f64dcc77783eddb01a81170e51a734
ThaisGO <thaisgo.viana@gmail.com>
2025-01-31 23:16:45 -0300

    first commit

---
+++ src/layout/AppFooter.vue
@@ -0,0 +1,93 @@
+<template>
+    <footer class="footer">
+        <div class="container">
+            <div class="row py-3">
+                <div class="col-md-4">
+                        <h3>How to Find Us</h3>
+
+                        <h5 class="mb-2">Headquarters</h5>
+                        <p class="m-0">9863 - 9867 MILL ROAD, CAMBRIDGE, MG09 99HT.</p>
+                        <p class="m-0">Telephon: +1 800 603 6035</p>
+                        <p class="mt-0 mb-2">E-mail: mail@demolink.org</p>
+                        
+                        <h5 class="mb-2">Headquarters</h5>
+                        <p class="m-0">9863 - 9867 MILL ROAD, CAMBRIDGE, MG09 99HT.</p>
+                        <p class="m-0">Telephon: +1 800 603 6035</p>
+                        <p class="m-0">E-mail: mail@demolink.org</p>
+                </div>
+
+                <div class="col-md-4">
+                        <h3>Navigation</h3>
+                        <ul class="list-unstyled mt-3">
+                                <li class="py-1">
+                                    <div class="d-flex align-items-center">
+                                        <badge type="success" circle class="mr-3" icon="ni ni-settings-gear-65"></badge>
+                                        <h6 class="mb-0">Carefully crafted components</h6>
+                                    </div>
+                                </li>
+                                <li class="py-2">
+                                    <div class="d-flex align-items-center">
+                                        <badge type="success" circle class="mr-3" icon="ni ni-html5"></badge>
+                                        <h6 class="mb-0">Amazing page examples</h6>
+                                    </div>
+                                </li>
+                        </ul>
+                </div>
+                <div class="col-md-4">
+                        <h3>Contact Us</h3>
+                        <div class="btn-wrapper">
+                                    <base-button 
+                                        tag="a"
+                                        href="https://demos.creative-tim.com/argon-design-system/docs/components/alerts.html"
+                                        class="mb-3 mb-sm-0"
+                                        type="success"
+                                        icon="fa fa-code">
+                                        Components
+                                    </base-button>
+                                </div>
+
+                </div>
+            </div>
+
+            <hr>
+
+            <div class="row align-items-center justify-content-md-between">
+                <div class="col-6">
+                    <div class="copyright">
+                        &copy; {{year}}
+                        <a href="https://www.creative-tim.com" target="_blank" rel="noopener">Creative Tim</a> & <a href="https://www.binarcode.com" target="_blank" rel="noopener">Binar Code</a>
+                    </div>
+                </div>
+
+                <div class="col-6 text-lg btn-wrapper">
+
+                    <!-- <div class="col-lg-6 text-lg btn-wrapper"> -->
+                        <ul class="nav nav-footer justify-content-end">
+
+                        <a target="_blank" rel="noopener" href="https://twitter.com/creativetim" data-toggle="tooltip" data-original-title="Follow us" class="btn btn-neutral btn-icon-only btn-twitter btn-round btn-lg">
+                            <i class="fa fa-twitter"></i>
+                        </a>
+                            
+                            <a target="_blank" rel="noopener" href="https://www.facebook.com/creativetim" data-toggle="tooltip" data-original-title="Like us" class="btn btn-neutral btn-icon-only btn-facebook btn-round btn-lg"><i class="fa fa-facebook-square"></i></a>
+                            
+                            <a target="_blank" rel="noopener" href="https://dribbble.com/creativetim" data-toggle="tooltip" data-original-title="Follow us" class="btn btn-neutral btn-icon-only btn-dribbble btn-lg btn-round"><i class="fa fa-dribbble"></i></a>
+                            
+                            <a target="_blank" rel="noopener" href="https://github.com/creativetimofficial" data-toggle="tooltip" data-original-title="Star on Github" class="btn btn-neutral btn-icon-only btn-github btn-round btn-lg"><i class="fa fa-github"></i></a>
+                        </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+<script>
+export default {
+  name: 'app-footer',
+  data() {
+    return {
+      year: new Date().getFullYear()
+    }
+  }
+};
+</script>
+<style>
+</style>
---
+++ src/views/Landing.vue
@@ -0,0 +1,296 @@
+<template>
+    <div>
+        <div class="position-relative">
+            <!-- shape Hero -->
+            <section class="section-shaped my-0 py-5">
+                <div class="shape shape-style-3 bg-gradient-default">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div class="container shape-container d-flex">
+                    <div class="col px-0">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <h1 class="display-3  text-gold">Liberdade Financeira
+                                    <span class="text-white">começa com boas decisões</span>
+                                </h1>
+                                <h4 class="lead  text-gold">Nós te ajudamos a toma-las</h4>
+
+                                <p class="lead  text-white">Com a nossa assessoria, você tem ao seu lado especialistas que entendem o mercado e sabem como transformar seu dinheiro em um verdadeiro aliado.</p>
+                                <div class="btn-wrapper">
+                                    <base-button tag="a"
+                                                 href="https://demos.creative-tim.com/argon-design-system/docs/components/alerts.html"
+                                                 class="mb-3 mb-sm-0"
+                                                 type="info"
+                                                 icon="fa fa-whatsapp">
+                                        Fale com um consultor
+                                    </base-button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+
+        <section class="section section-shaped my-0 overflow-hidden">
+            <div class="shape shape-style-3 bg-gradient-default shape-skew">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div class="container pb-300">
+                <div class="row text-center justify-content-center">
+                    <div class="col-lg-10">
+                        <h2 class="display-3 text-white">Serviços</h2>
+                        <p class="lead text-white">According to the National Oceanic and Atmospheric Administration,
+                            Ted, Scambos, NSIDClead scentist, puts the potentially record low maximum sea ice extent
+                            tihs year down to low ice.</p>
+                    </div>
+                </div>
+
+                <div class="row row-grid mt-5">
+                    <div class="col-lg-4">
+                        <icon name="ni ni-settings" size="lg" gradient="white" shadow round color="primary"></icon>
+                        <h5 class="text-white mt-3">Planejamento Financeiro</h5>
+                        <p class="text-white mt-3">Alcance suas metas com segurança através de um planejamento personalizado.</p>
+                    </div>
+                    <div class="col-lg-4">
+                        <icon name="ni ni-ruler-pencil" size="lg" gradient="white" shadow round color="primary"></icon>
+                        <h5 class="text-white mt-3">Grow your market</h5>
+                        <p class="text-white mt-3">Some quick example text to build on the card title and make up the
+                            bulk of the card's content.</p>
+                    </div>
+                    <div class="col-lg-4">
+                        <icon name="ni ni-atom" size="lg" gradient="white" shadow round color="primary"></icon>
+                        <h5 class="text-white mt-3">Launch time</h5>
+                        <p class="text-white mt-3">Some quick example text to build on the card title and make up the
+                            bulk of the card's content.</p>
+                    </div>
+                </div>
+                <div class="row row-grid mt-5">
+                    <div class="col-lg-4">
+                        <icon name="ni ni-settings" size="lg" gradient="white" shadow round color="primary"></icon>
+                        <h5 class="text-white mt-3">Building tools</h5>
+                        <p class="text-white mt-3">Some quick example text to build on the card title and make up the
+                            bulk of the card's content.</p>
+                    </div>
+                    <div class="col-lg-4">
+                        <icon name="ni ni-ruler-pencil" size="lg" gradient="white" shadow round color="primary"></icon>
+                        <h5 class="text-white mt-3">Grow your market</h5>
+                        <p class="text-white mt-3">Some quick example text to build on the card title and make up the
+                            bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section">
+            <div class="container">
+                <div class="row row-grid align-items-center">
+                    <div class="col-md-6 order-md-1 order-sm-2">
+                        <img src="img/theme/promo-1.png" class="img-fluid floating">
+                    </div>
+                    
+                    <div class="col-md-6 order-md-2 order-sm-1">
+                        <div >
+                            <h3>Awesome features</h3>
+                            <p>The kit comes with three pre-built pages to help you get started faster. You can change
+                                the text and images and you're good to go.</p>
+                            <ul class="list-unstyled mt-3">
+                                <li class="py-1">
+                                    <div class="d-flex align-items-center">
+                                        <badge type="success" circle class="mr-3" icon="ni ni-settings-gear-65"></badge>
+                                        <h6 class="mb-0">Carefully crafted components</h6>
+                                    </div>
+                                </li>
+                                <li class="py-2">
+                                    <div class="d-flex align-items-center">
+                                        <badge type="success" circle class="mr-3" icon="ni ni-html5"></badge>
+                                        <h6 class="mb-0">Amazing page examples</h6>
+                                    </div>
+                                </li>
+                                <li class="py-2">
+                                    <div class="d-flex align-items-center">
+                                        <badge type="success" circle class="mr-3" icon="ni ni-satisfied"></badge>
+                                        <h6 class="mb-0">Super friendly support team</h6>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section">
+            <div class="container">
+                <div class="row text-center justify-content-center pb-4">
+                    <div class="col-lg-10">
+                        <h2 class="display-3">Testmonials</h2>
+                        <p class="lead">What our clients talk about us.</p>
+                    </div>
+                </div>
+                
+                <div class="row justify-content-center">
+                    <div class="col-lg-12">
+                        <div class="row row-grid">
+                            <div class="col-lg-4">
+                                <card class="border-0" hover shadow body-classes="py-5">
+                                    <icon name="ni ni-check-bold" type="primary" rounded class="mb-4">
+                                    </icon>
+                                    <h6 class="text-primary text-uppercase">Download Argon</h6>
+                                    <p class="description mt-3">Argon is a great free UI package based on Bootstrap 4
+                                        that includes the most important components and features.</p>
+                                    <div>
+                                        <badge type="primary" rounded>design</badge>
+                                        <badge type="primary" rounded>system</badge>
+                                        <badge type="primary" rounded>creative</badge>
+                                    </div>
+                                    <base-button tag="a" href="#" type="primary" class="mt-4">
+                                        Learn more
+                                    </base-button>
+                                </card>
+                            </div>
+                            <div class="col-lg-4">
+                                <card class="border-0" hover shadow body-classes="py-5">
+                                    <icon name="ni ni-istanbul" type="success" rounded class="mb-4">
+                                    </icon>
+                                    <h6 class="text-success text-uppercase">Build Something</h6>
+                                    <p class="description mt-3">Argon is a great free UI package based on Bootstrap
+                                        4 that includes the most important components and features.</p>
+                                    <div>
+                                        <badge type="success" rounded>business</badge>
+                                        <badge type="success" rounded>vision</badge>
+                                        <badge type="success" rounded>success</badge>
+                                    </div>
+                                    <base-button tag="a" href="#" type="success" class="mt-4">
+                                        Learn more
+                                    </base-button>
+                                </card>
+                            </div>
+                            <div class="col-lg-4">
+                                <card class="border-0" hover shadow body-classes="py-5">
+                                    <icon name="ni ni-planet" type="warning" rounded class="mb-4">
+                                    </icon>
+                                    <h6 class="text-warning text-uppercase">Prepare Launch</h6>
+                                    <p class="description mt-3">Argon is a great free UI package based on Bootstrap
+                                        4 that includes the most important components and features.</p>
+                                    <div>
+                                        <badge type="warning" rounded>marketing</badge>
+                                        <badge type="warning" rounded>product</badge>
+                                        <badge type="warning" rounded>launch</badge>
+                                    </div>
+                                    <base-button tag="a" href="#" type="warning" class="mt-4">
+                                        Learn more
+                                    </base-button>
+                                </card>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <section class="section bg-secondary">
+            <div class="container">
+                <div class="row row-grid align-items-center">
+                    <div class="col-md-6">
+                        <div class="card bg-default shadow border-0">
+                            <img v-lazy="'img/theme/img-1-1200x1000.jpg'" class="card-img-top">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="pl-md-5">
+                            <h3>Our customers</h3>
+                            <p class="lead">Don't let your uses guess by attaching tooltips and popoves to any element.
+                                Just make sure you enable them first via JavaScript.</p>
+                            <p>The kit comes with three pre-built pages to help you get started faster. You can change
+                                the text and images and you're good to go.</p>
+                            <p>The kit comes with three pre-built pages to help you get started faster. You can change
+                                the text and images and you're good to go.</p>
+                            <a href="#" class="font-weight-bold text-warning mt-5">A beautiful UI Kit for impactful
+                                websites</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <section class="section section-lg pt-0">
+            <div class="container">
+                <card gradient="warning"
+                      no-body
+                      shadow-size="lg"
+                      class="border-0">
+                    <div class="p-5">
+                        <div class="row align-items-center">
+                            <div class="col-lg-8">
+                                <h3 class="text-white">We made investments easier for you.</h3>
+                                <p class="lead text-white mt-3">I will be the leader of a company that ends up being
+                                    worth billions of dollars, because I got the answers. I understand culture.</p>
+                            </div>
+                            <div class="col-lg-3 ml-lg-auto">
+                                <base-button tag="a" href="https://www.creative-tim.com/product/vue-argon-design-system" type="white" block size="lg">
+                                    Contact here
+                                </base-button>
+                            </div>
+                        </div>
+                    </div>
+                </card>
+            </div>
+        </section>
+
+        <section class="section pt-0">
+            <div class="container">
+                <div class="row text-center justify-content-center">
+                    <div class="col-lg-10">
+                        <h2 class="display-3">Contact</h2>
+                        <h4 class="mb-1">Want to work with us?</h4>
+                        <p class="mt-0">Your project is very important to us.</p>
+                    </div>
+                </div>
+
+                <div class="row justify-content-center">
+                    <div class="col-lg-10">
+                        <card gradient="transparent" shadow body-classes="p-lg-5">
+                            <base-input class="mt-5"
+                                        alternative
+                                        placeholder="Your name"
+                                        addon-left-icon="ni ni-user-run">
+                            </base-input>
+                            <base-input alternative
+                                        placeholder="Email address"
+                                        addon-left-icon="ni ni-email-83">
+                            </base-input>
+                            <base-input class="mb-4">
+                                    <textarea class="form-control form-control-alternative" name="name" rows="4" cols="80" placeholder="Type a message..."></textarea>
+                            </base-input>
+                            <base-button type="default" round block size="lg">
+                                Send Message
+                            </base-button>
+                        </card>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "home",
+    components: {}
+};
+</script>
